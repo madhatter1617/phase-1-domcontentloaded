0e5f737e65a212e6497deff66c58c26276488753
Rosa <rosarenee@att.net>
2021-05-24 00:32:07 -0500

    changed the text but fials test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+ 
+    document.getElementById('text').innerText =  "This is really cool!";
+
+
+
+
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  ); 
